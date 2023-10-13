d3a7c19d8d8b0d440d772ffa77a033ae5655c504
Habiba Hesham <habibahshm@gmail.com>
2023-10-13 22:04:50 +0900

    eye tracking first attempt

---
+++ Assets/EyeTracker.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//[RequireComponent(typeof(LineRenderer))]
+
+public class EyeTracker : MonoBehaviour
+{
+    public float rayDistance = 1.0f;
+    public float rayWidth = 0.01f;
+    public LayerMask layersToinclude;
+
+    public Color rayColor = Color.red;
+    public GameObject rectilePrefab;
+    GameObject rectile;
+
+    //private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        //lineRenderer = GetComponent<LineRenderer>();
+        //SetupRay();
+        rectile = null;
+    }
+
+   /* void SetupRay()
+    {
+        lineRenderer.useWorldSpace = false; // why?
+        lineRenderer.positionCount = 2;
+        lineRenderer.startWidth = rayWidth;
+        lineRenderer.endWidth = rayWidth;
+        lineRenderer.startColor = rayColor;
+        lineRenderer.endColor = rayColor;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, transform.position + new Vector3(0, 0, rayDistance));
+
+    }*/
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layersToinclude))
+        {
+            if(rectile == null)
+            {
+                rectile = Instantiate(rectilePrefab, hit.point, rectilePrefab.transform.rotation);
+            }
+            else
+            {
+                rectile.transform.position = hit.point;
+            }
+            
+        }
+    }
+
+
+}
